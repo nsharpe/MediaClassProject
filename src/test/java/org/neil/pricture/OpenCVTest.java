839ec6fea80cb20d34582911ef01336ab6dec0e1
Neil Sharpe <nsharpe@gmail.com>
2015-10-27 20:55:54 -0700

    add open cv test

--- src/test/java/org/neil/pricture/OpenCVTest.java
+++ src/test/java/org/neil/pricture/OpenCVTest.java
@@ -1,6 +1,12 @@
 package org.neil.pricture;
 
 import org.junit.Test;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.Scalar;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 /**
  * Created by Neil on 10/26/2015.
@@ -8,7 +14,20 @@ import static org.junit.Assert.assertTrue;
 public class OpenCVTest {
 
     @Test
+    public void testOpenCVInstalled(){
+        System.out.println("Welcome to OpenCV " + Core.VERSION);
+        Mat m = new Mat(5, 10, CvType.CV_8UC1, new Scalar(0));
+        System.out.println("OpenCV Mat: " + m);
+        Mat mr1 = m.row(1);
+        mr1.setTo(new Scalar(1));
+        Mat mc5 = m.col(5);
+        mc5.setTo(new Scalar(5));
+        System.out.println("OpenCV Mat data:\n" + m.dump());
+    }
+
+    @Test
     public void testBasicOpenCV(){
         assertTrue(true);
+        assertEquals(true,true);
     }
 }
