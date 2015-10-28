41f1c670c98f2fc27a535bb6cd3e7dc943424bad
Neil Sharpe <nsharpe@gmail.com>
2015-10-27 19:39:30 -0700

    Initial pom setup

---
+++ src/test/java/org/neil/pricture/OpenCVTest.java
@@ -0,0 +1,14 @@
+package org.neil.pricture;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+/**
+ * Created by Neil on 10/26/2015.
+ */
+public class OpenCVTest {
+
+    @Test
+    public void testBasicOpenCV(){
+        assertTrue(true);
+    }
+}
